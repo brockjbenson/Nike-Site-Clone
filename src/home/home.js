f4983624d1f76073ee7de07d8e6afb32af7b1d30
Brock Benson <brockjbenson@outlook.com>
2023-09-14 22:01:17 -0500

    middle right nav finished

--- src/home/home.js
+++ src/home/home.js
@@ -1,1 +1,9 @@
-console.log("js");
+// Search Container Click Listener
+
+document.addEventListener("click", (e) => {
+  let searchContainer = document.getElementsByClassName("nav-search");
+  let input = document.getElementsByTagName("input");
+  if (e.target === searchContainer[0]) {
+    input[0].focus();
+  }
+});
